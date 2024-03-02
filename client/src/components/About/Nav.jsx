f442f35194afac4e2430a7debb1fefd396d248a8
Yogananda1504 <vynr1504@gmail.com>
2024-03-02 16:23:31 +0530

    another commit

--- client/src/components/About/Nav.jsx
+++ client/src/components/About/Nav.jsx
@@ -2,15 +2,18 @@ import React from 'react'
 import './Nav.css';
 function Nav() {
   return (
- 
-       <nav className='NavBar'>
-        <div className="logo">logo</div>
-        <div className="About">About</div>
+
+    <nav className='NavBar'>
+      <div className="logo">logo</div>
+      <div className="About">About</div>
+      <div className='ls' >
         <div className="Login">Login</div>
         <div className="SignUp">SignUp</div>
-        
-        </nav> 
-    
+      </div>
+
+
+    </nav>
+
   )
 }
 
